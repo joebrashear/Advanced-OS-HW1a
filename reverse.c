317c9d7b64f66642ee23c14c9dd46f29865cf90e
joebrashear <joebrashear@live.com>
2023-01-21 18:54:27 -0500

    Adding file inputs and outputs and handling edge cases

--- reverse.c
+++ reverse.c
@@ -1,1 +1,34 @@
-//Your code goes here..!
+#include <stdio.h>
+
+void reverse() 
+{
+
+}
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 2) 
+    {
+        fprintf(stderr, "reverse <input> <output>");
+        exit(1);
+    }
+
+    FILE *input = fopen(argv[0], "r");
+
+    if (input == NULL) 
+    {
+        fprintf(stderr, "reverse: cannot open file 'input.txt'");
+        exit(1);
+    }
+
+    FILE *output = fopen(argv[1], "r+");
+
+    if (output == NULL) 
+    {
+        fprintf(stderr, "reverse: cannot open file 'output.txt'");
+        fclose(input);
+        exit(1);
+    }
+
+    return 0;
+}
