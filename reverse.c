3beae5e43a82092c5fa7062359931edeff50d921
joebrashear <joebrashear@live.com>
2023-01-27 18:29:47 -0500

    Fix issues, begin to handle different inputs

--- reverse.c
+++ reverse.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
 int debug = 0;
 
@@ -31,23 +32,18 @@ int is_same(FILE *input, FILE *output)
         }
     }
 
-    fprintf(stderr, "Input and output file must differ");
+    fprintf(stderr, "Input and output file must differ\n");
     return 1;
 }
 
 void reverse_to_output(ListNode *head, FILE *output)
 {
     ListNode *temp = NULL;
-    int node_count = 1;
+
     // add to output and deallocate LL
     while (head != NULL)
     {
         fprintf(output, "%s", head->text);
-        if (node_count == 1)
-        {
-            fprintf(output, "\n");
-            node_count++;
-        }
         temp = head;
         head = head->next;
         free(temp);
@@ -99,7 +95,7 @@ int read_text(char *text, size_t len, FILE *input, ListNode *node, ListNode *hea
 
         if (node->next == NULL) 
         {
-            fprintf(stderr, "Malloc has failed");
+            fprintf(stderr, "Malloc has failed\n");
             free(text);
             deallocate_List(head);
             return 1;
@@ -109,7 +105,7 @@ int read_text(char *text, size_t len, FILE *input, ListNode *node, ListNode *hea
 
         if (node->next->text == NULL)
         {
-            fprintf(stderr, "Malloc has failed");
+            fprintf(stderr, "Malloc has failed\n");
             free(text);
             deallocate_List(head);
             return 1;
@@ -137,7 +133,7 @@ int reverse(FILE *output, FILE *input)
 
     if (node == NULL) 
     {
-        fprintf(stderr, "Malloc has failed");
+        fprintf(stderr, "Malloc has failed\n");
         return 1;
     }
 
@@ -161,42 +157,79 @@ int reverse(FILE *output, FILE *input)
 
 int main(int argc, char const *argv[])
 {
-    FILE *input;
-    FILE *output;
+    FILE *input = NULL;
+    FILE *output = NULL;
+    struct stat input_stat;
+    int input_stat_ret = 0;
+    struct stat output_stat;
+    int output_stat_ret = 0;
 
-    if (argc != 3) 
+    if (argc > 3 || argc < 1) 
     {
-        fprintf(stderr, "reverse <input> <output>");
+        fprintf(stderr, "usage: reverse <input> <output>\n");
         exit(1);
     }
 
-    input = fopen(argv[1], "r");
+    if (argc >= 1)
+        input = fopen(argv[1], "r");
+    else
+    {
+        char *file_name = NULL;
+        scanf("%s", file_name);
+        input = fopen(file_name, "r");
+    }
 
     if (input == NULL) 
     {
-        fprintf(stderr, "reverse: cannot open file '%s'", argv[1]);
+        fprintf(stderr, "reverse: cannot open file '%s'\n", argv[1]);
         exit(1);
     }
 
-    output = fopen(argv[2], "r+");
-
-    if (output == NULL) 
+    if (argc == 3) 
     {
-        fprintf(stderr, "reverse: cannot open file '%s'", argv[2]);
-        fclose(input);
-        exit(1);
-    }
+        output = fopen(argv[2], "w+");
 
+        if (output == NULL) 
+        {
+            fprintf(stderr, "reverse: cannot open file '%s'\n", argv[2]);
+            fclose(input);
+            exit(1);
+        }
+
+        input_stat_ret = stat(argv[1], &input_stat);
+
+        if (input_stat_ret < 0)
+        {
+            fprintf(stderr, "Cannot get input file stat\n");
+            fclose(input);
+            fclose(output);
+            exit(1);
+        }
+
+        output_stat_ret = stat(argv[2], &output_stat);
+
+        if (output_stat_ret < 0)
+        {
+            fprintf(stderr, "Cannot get output file stat\n");
+            fclose(input);
+            fclose(output);
+            exit(1);
+        }
+
+        if (input_stat.st_ino == output_stat.st_ino)
+        {
+            fprintf(stderr, "reverse: input and output file must differ\n");
+            fclose(input);
+            fclose(output);
+            exit(1);
+        }
+    }
+    else
+    {
+        output = stdout;
+    }
     
-
     if (reverse(output, input) == 1) 
-    {
-        fclose(input);
-        fclose(output);
-        exit(1);
-    }
-
-    if (is_same(input, output) == 1) 
     {
         fclose(input);
         fclose(output);
